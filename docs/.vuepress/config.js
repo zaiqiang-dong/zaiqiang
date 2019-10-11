628d212eb3b6bb6ade74c2d1ac9ee7ca1aaeee2e
Zaiqiang Dong <zqdong@nreal.ai>
2019-10-11 20:53:18 +0800

    modify blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,7 +34,7 @@ module.exports = {
             after: '</div>'
         }],
         ['@vuepress/google-analytics',{
-            'ga': 'UA-149833085-2'
+            'ga': 'UA-149833085-3'
         }],
         ['@vuepress/back-to-top'],
         ['@vuepress/last-updated'],
