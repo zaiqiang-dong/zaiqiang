4347accf5df914666293d6de56224fb20e882440
zqdong <zqdong@nreal.ai>
2019-11-05 11:49:12 +0800

    modify blog

--- install.sh
+++ install.sh
@@ -4,8 +4,6 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
 
-export PATH="$PATH:~/.yarn/bin"
-
 yarn global add vuepress
 yarn global add vuepress-plugin-container
 yarn global add @vuepress/plugin-back-to-top -D
