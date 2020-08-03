67f4449108926a50c6a54dee913bf921805a75cf
Zaiqiang dong <zqdong@nreal.ai>
2020-08-03 18:25:09 +0800

    modify blog

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt-get update && sudo apt-get install yarn
 
 yarn global add vuepress
-yarn global  add vuepress-plugin-mathjax
+yarn global add vuepress-plugin-mathjax
 yarn global add vuepress-plugin-container
 yarn global add @vuepress/plugin-back-to-top -D
 yarn global add @vuepress/plugin-google-analytics -D
+yarn global add @vssue/api-github-v3
+yarn global add @vssue/vuepress-plugin-vssue
